5c1b84b6de74bba91bf104e7e24829d069aedf31
opilane <opilane@HP-CZC6297B8P>
2022-10-04 14:16:35 +0300

    sobitamine

---
+++ rakendusteLoomine_tulusa/koikVormid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rakendusteLoomine_tulusa
+{
+    public partial class koikVormid : Form
+    {
+        Button pilt, matem, parid;
+        public koikVormid()
+        {
+            this.Size = new System.Drawing.Size(600, 300);
+            BackColor = Color.LightBlue;
+            pilt = new Button()
+            {
+                Text = "pildid",
+                Location=new System.Drawing.Point(100,125),
+                Size = new System.Drawing.Size(100, 50),
+                BackColor = Color.LightYellow,
+            };
+            matem = new Button()
+            {
+                Text="matemaatika",
+                Location = new System.Drawing.Point(250, 125),
+                Size = new System.Drawing.Size(100, 50),
+                BackColor = Color.LightCoral,
+            };
+            parid = new Button()
+            {
+                Text="parid",
+                Location = new System.Drawing.Point(400, 125),
+                Size = new System.Drawing.Size(100, 50),
+                BackColor = Color.LightGoldenrodYellow,
+            };
+            pilt.Click += Pilt_Click;
+            this.Controls.Add(pilt);
+            this.Controls.Add(matem);
+            this.Controls.Add(parid);
+        }
+
+        private void Pilt_Click(object sender, EventArgs e)
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new piltideVaataja());
+        }
+    }
+}
---
+++ rakendusteLoomine_tulusa/sobitamine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rakendusteLoomine_tulusa
+{
+    public partial class sobitamine : Form
+    {
+        public sobitamine()
+        {
+            
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+            this.Size = new Size(900, 900);
+            tableLayoutPanel = new TableLayoutPanel
+            {
+                AutoSize = true,
+                ColumnCount = 4,
+                RowCount = 4,
+                Location = new System.Drawing.Point(45, 30),
+                BackColor = System.Drawing.Color.LightSteelBlue,
+                Size = new Size(800,800)
+            };
+            
+            this.Controls.Add(tableLayoutPanel);
+        }
+    }
+}
